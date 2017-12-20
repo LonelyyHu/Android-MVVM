639c951f51b9c943eefe3dfc71116d5f48381e79
hulonelyy <lonelyy.tw@gmail.com>
2017-12-21 07:08:43 +0800

    add task model

---
+++ app/src/main/java/com/lonelyyhu/exercise/android_mvvm/data/Task.kt
@@ -0,0 +1,16 @@
+package com.lonelyyhu.exercise.android_mvvm.data
+
+/**
+ * Created by hulonelyy on 2017/12/21.
+ */
+class Task {
+
+    lateinit var id: String
+
+    lateinit var title: String
+
+    lateinit var description: String
+
+    var complete = false
+
+}
--- app/src/main/java/com/lonelyyhu/exercise/android_mvvm/ui/TasksViewModel.kt
+++ app/src/main/java/com/lonelyyhu/exercise/android_mvvm/ui/TasksViewModel.kt
@@ -1,8 +1,10 @@
 package com.lonelyyhu.exercise.android_mvvm.ui
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableArrayList
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
+import com.lonelyyhu.exercise.android_mvvm.data.Task
 
 /**
  * Created by hulonelyy on 2017/12/20.
@@ -20,6 +22,8 @@ class TasksViewModel() : ViewModel() {
         @JvmField
         var obStrFilteringLabel = ObservableField<String>()
 
+        @JvmField
+        var obListTasks = ObservableArrayList<Task>()
 
 
 
